388e51c0c461caa015ccbac7e29692d438d53ae3
SeReN3247 <88083927+SeReN3247@users.noreply.github.com>
2023-11-20 21:10:00 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const mongourl = 'mongodb+srv://KasenLin:Bjklsad123@stockmanagement.otjjmtt.mongodb.net/'; 
+const mongourl = 'mongodb+srv://Kasen:Bjklsad123@test.brdufts.mongodb.net/?retryWrites=true&w=majority'; 
 const dbName = 'test';
 
 const express = require('express');
